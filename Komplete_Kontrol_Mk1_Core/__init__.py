2084e63c04a8a840a212a33f090d809e61e79071
Brad Osgood <bosgood@gmail.com>
2017-10-08 16:39:13 -0400

    Share code between 2 device roles

---
+++ Komplete_Kontrol_Mk1_Core/__init__.py
@@ -0,0 +1,28 @@
+#__init__.py
+from FocusControl import FocusControl, log, DEVICE_ROLE_DAW, DEVICE_ROLE_MIDI_KEYBOARD
+
+
+def create_midi_instance(c_instance):
+    log('Komplete_Kontrol creating instance')
+    return FocusControl(c_instance, DEVICE_ROLE_MIDI_KEYBOARD)
+
+
+def create_daw_instance(c_instance):
+    log('Komplete_Kontrol creating instance')
+    return FocusControl(c_instance, DEVICE_ROLE_DAW)
+
+
+from _Framework.Capabilities import *
+
+
+def get_capabilities():
+    return {
+        CONTROLLER_ID_KEY: controller_id(vendor_id=9000, product_ids=[
+            2], model_name='Focus Control'),
+        PORTS_KEY: [inport(props=[HIDDEN, NOTES_CC, SCRIPT]),
+                    inport(props=[]),
+                    outport(props=[HIDDEN,
+                                   NOTES_CC,
+                                   SYNC,
+                                   SCRIPT]),
+                    outport(props=[])]}
